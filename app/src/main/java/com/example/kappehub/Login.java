b2e503b3abd35aadc758385575d74dc89732cb10
Rhea Tamera <rhea.tamerarj@gmail.com>
2022-12-08 22:24:42 +0800

    linked registration and login together

--- app/src/main/java/com/example/kappehub/Login.java
+++ app/src/main/java/com/example/kappehub/Login.java
@@ -2,15 +2,32 @@ package com.example.kappehub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
+    Button callSignUp;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        callSignUp = findViewById(R.id.signup_screen);
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,Registration.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/kappehub/Registration.java
+++ app/src/main/java/com/example/kappehub/Registration.java
@@ -2,13 +2,27 @@ package com.example.kappehub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registration extends AppCompatActivity {
 
+    Button callLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        callLogin = findViewById(R.id.login_screen);
+
+        callLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Registration.this,Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
