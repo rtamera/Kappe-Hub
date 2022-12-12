aafa7ca408cdac6f7591de85bbc769470a013ea0
Rhea Tamera <rhea.tamerarj@gmail.com>
2022-12-12 18:34:07 +0800

    added earn points button

--- app/src/main/java/com/example/kappehub/Home.java
+++ app/src/main/java/com/example/kappehub/Home.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
     ArrayList<CafeModel> cafeModels = new ArrayList<>();
     int[] cafeImages ={R.drawable.ic_baseline_boy_24, R.drawable.ic_baseline_coffee_24,R.drawable.ic_baseline_currency_exchange_24};
-
+    Button earnPoints;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,15 @@ public class Home extends AppCompatActivity {
         Cafe_RecyclerViewAdapter adapter = new Cafe_RecyclerViewAdapter(this, cafeModels);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        earnPoints = findViewById(R.id.earn_points_btn);
+        earnPoints.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, QRScanner.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void setUpCafeModel(){
@@ -35,4 +47,6 @@ public class Home extends AppCompatActivity {
             cafeModels.add(new CafeModel(cafeNames[i],time[i],rating[i],cafeImages[i]));
         }
     }
+
+
 }
