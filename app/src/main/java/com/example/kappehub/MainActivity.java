74bf20f1eee3d7aa9e59127c5fd90a0b5f4783e8
Rhea Tamera <rhea.tamerarj@gmail.com>
2022-12-12 13:34:19 +0800

    works on main activity not home

--- app/src/main/java/com/example/kappehub/MainActivity.java
+++ app/src/main/java/com/example/kappehub/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kappehub;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,9 +13,13 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private static int SPLASH_SCREEN = 4000;
+    ArrayList<CafeModel> cafeModels = new ArrayList<>();
+    int[] cafeImages ={R.drawable.ic_baseline_boy_24, R.drawable.ic_baseline_coffee_24,R.drawable.ic_baseline_currency_exchange_24};
 
     //Variables
     Animation topAnim, bottomAnim;
@@ -23,8 +29,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
+
+        RecyclerView recyclerView = findViewById(R.id.mRecyclerView);
+
+        setUpCafeModel();
+
+        Cafe_RecyclerViewAdapter adapter = new Cafe_RecyclerViewAdapter(this, cafeModels);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void setUpCafeModel(){
+        String[] cafeNames = getResources().getStringArray(R.array.cafe_name);
+        String[] time = getResources().getStringArray(R.array.time);
+        String[] rating = getResources().getStringArray(R.array.rating);
+
+        for(int i = 0; i <cafeNames.length; i++){
+            cafeModels.add(new CafeModel(cafeNames[i],time[i],rating[i],cafeImages[i]));
+        }
+    }
 
 //        //Animations
 //        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_anim);
@@ -54,4 +78,3 @@ public class MainActivity extends AppCompatActivity {
 //    }
     }
 
-}
